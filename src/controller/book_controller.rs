4f89c21e5c4a8291f38f1fd12bce466ea81a4921
litleSnow <lijiaxin@xhblog.top>
2024-12-13 22:12:37 +0800

    完成爬虫创建书本接口

--- src/controller/book_controller.rs
+++ src/controller/book_controller.rs
@@ -1,10 +1,10 @@
 use actix_web::{post, web, HttpResponse, Responder};
-
+use crate::model::response;
 use crate::service::book_services::BookServices;
 #[post("/add/{book_id}")]
 async fn add_book(book_id: web::Path<i32>) -> impl Responder {
     match BookServices::add_sf_book(*book_id).await { // Dereference book_id to get the i32 value
-        Ok(book) => HttpResponse::Ok().json(book), // 成功时返回 JSON 响应
+        Ok(book) => HttpResponse::Ok().json(response::ResponseOk::new(book)), // 成功时返回 JSON 响应
         Err(err) => {
             HttpResponse::InternalServerError().json(err.to_string())
         } // 错误时返回错误信息
