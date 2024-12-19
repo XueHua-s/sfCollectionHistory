4df191bdf023bd76dfa553fe149d36fc0896ac37
litleSnow <lijiaxin@xhblog.top>
2024-12-19 16:50:53 +0800

    feature: 增加组件库颜色配置上下文

---
+++ clinet/src/components/SFConfigProvider.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import zhCN from "antd/locale/zh_CN";
+import { ConfigProvider } from 'antd'
+interface SFConfigProviderProps {
+  children: React.ReactNode;
+}
+const SFConfigProvider: React.FC<SFConfigProviderProps> = ({ children
+}) => {
+  return <ConfigProvider
+    locale={zhCN}
+    theme={{
+      token: {
+        // Seed Token，影响范围大
+        colorPrimary: '#e29464',
+        borderRadius: 2,
+        // 派生变量，影响范围小
+        colorBgContainer: '#faf9f9',
+      },
+      components: {
+      },
+    }}>
+    {children}
+  </ConfigProvider>
+}
+export default SFConfigProvider
