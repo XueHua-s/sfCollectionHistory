65ef65c3092354391e619be76c3a36990628cb86
litleSnow <lijiaxin@xhblog.top>
2024-12-26 15:36:22 +0800

    feature: 增加百度头

--- clinet/src/app/layout.tsx
+++ clinet/src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="zh-cn">
     <head>
       <meta charSet="utf-8"/>
       <meta name="baidu-site-verification" content="codeva-FWMMQRBATs"/>
